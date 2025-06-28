be6a12e39b300523767902b1bf0a990a43dd64bb
Muneer Ali <alimuneerali245@gmail.com>
2025-06-29 05:27:34 +0530

    Build and Deploy Setup

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
     images: { unoptimized: true },
+    output: 'standalone',
 };
 
 module.exports = nextConfig;
